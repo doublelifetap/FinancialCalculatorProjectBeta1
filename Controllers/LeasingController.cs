b8c0831b74d962c44889cc9a1952bf529ab9b17f
Ivan Panev <hristopanev@outlook.com>
2021-02-09 16:34:30 +0200

    push1

---
+++ Controllers/LeasingController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using PartyInvites.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PartyInvites.Controllers
+{
+    public class LeasingController : Controller
+    {
+        [HttpGet]
+        public IActionResult LeaseCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //public IActionResult Index(LeasingModel LeasingModel, string calculate)
+        public IActionResult LeaseCalc(string LeaseAmount, string InitialPayment, string LeasePeriodMonths, string MonthlyPayments, string InitialFilingFee, bool FlatOrPercent)
+        {
+            try
+            {
+                /* verifies all inputs are positive */
+                if (int.Parse(LeaseAmount) < 0 || int.Parse(InitialPayment) < 0 || int.Parse(LeasePeriodMonths) < 0 || int.Parse(MonthlyPayments) < 0 || int.Parse(InitialFilingFee) < 0)
+                {
+                    ViewBag.NegNumError = "Please input positive numbers";
+                }
+
+                else {
+                    /* calculates if InitialFee has been selected as a percentage */
+                    /* TEMP; Should be if (FlatOrPercent == true) */
+                    if (true == true)
+                    {
+                        double leaseAmountDouble = double.Parse(LeaseAmount);
+                        double initialPaymentDouble = double.Parse(InitialPayment);
+                        double leasePeriodMonthsDouble = double.Parse(LeasePeriodMonths);
+                        double monthlyPaymentsDouble = double.Parse(MonthlyPayments);
+                        double initialFilingFeeDouble = double.Parse(InitialFilingFee);
+
+                        double Tax = initialFilingFeeDouble;
+
+                        /*Taxes calc %:*/
+                        double totalTaxes = leaseAmountDouble * (initialFilingFeeDouble / 100);
+
+                        /*Taxes calc FLAT: 
+                         double totalTaxes = initialFilingFeeDouble;
+                        */
+
+                        double totalPaid = Tax + initialPaymentDouble + monthlyPaymentsDouble * leasePeriodMonthsDouble;
+
+                        ViewBag.totalPaidWithTaxes = totalPaid + totalTaxes;
+                        ViewBag.totalTaxes = totalTaxes;
+
+                        double oneTooMany = ViewBag.totalPaidWithTaxes - leaseAmountDouble;
+                        /*Изчислявам го като процен оскъпяване спрямо цената на стоката*/
+                        ViewBag.totalYearlySpendingPercent = (oneTooMany / totalPaid) * 100;
+
+
+                        return View();
+                    }
+
+                /* calculates if InitialFee has been selected as a flat value */
+                    else
+                    {
+                        double leaseAmountDouble = double.Parse(LeaseAmount);
+                        double initialPaymentDouble = double.Parse(InitialPayment);
+                        double leasePeriodMonthsDouble = double.Parse(LeasePeriodMonths);
+                        double monthlyPaymentsDouble = double.Parse(MonthlyPayments);
+                        double initialFilingFeeDouble = double.Parse(InitialFilingFee);
+
+                        double Tax = leaseAmountDouble * (initialFilingFeeDouble / 100);
+                        double totalPaid = Tax + initialPaymentDouble + monthlyPaymentsDouble * leasePeriodMonthsDouble;
+
+                            return View();
+                    }
+                }
+
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                string emptyValueError = ex.Message;
+                ViewBag.ErrorMessageEmpty = "Please input a number and try again";
+            }
+            catch (FormatException ex)
+            {
+                string formatError = ex.Message;
+                ViewBag.ErrorFormat = "Please enter numeric values only and try again";
+            }
+
+
+            return View();
+            /* if (lease == "calculate")
+            {
+                //LeasingModel.LeasingTotalResult = LeasingModel.LeasingValue1 + LeasingModel.LeasingValue2 + LeasingModel.LeasingValue3;
+            }
+            else
+            {
+               // LeasingModel.LeasingTotalResult = LeasingModel.LeasingValue1 + LeasingModel.LeasingValue2 - LeasingModel.LeasingValue3;
+            }
+
+            return View(LeasingModel);*/
+
+        }
+
+    }
+}
---
+++ Models/RefinancingModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+
+    public class RefinanceModel
+    {
+
+        [Required(ErrorMessage = "Please enter first value")]
+        public int Value1 { get; set; }
+        [Required(ErrorMessage = "Please enter second value")]
+        public int Value2 { get; set; }
+        [Required(ErrorMessage = "Please enter third value")]
+        public int Value3 { get; set; }
+        public int RefinanceResult { get; set; }
+        public bool? AddOrSubstract { get; set; }
+
+        /*public void CalculateResult(){
+            RefinanceResult = Value1 + Value2 + Value3;
+        }*/
+
+
+
+    }
+}
+//<div asp-validation-summary="All"></div> to Ref Calc Top Body
